c2ea89542ebbb4824077e3095732210634c4e628
Elshad98 <safarov.elshad007@gmail.com>
2020-07-21 18:54:55 +0500

    Added constants for the app

---
+++ app/src/main/java/com/example/weather/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.example.weather.utils;
+
+public class Constants {
+
+    public static final String BASE_URL = "https://api.openweathermap.org/data/2.5";
+
+    public static final String API_KEY = "35eb36d6c29f1e2cc642273d12bc021a";
+
+}

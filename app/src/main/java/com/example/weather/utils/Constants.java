cd18cc3dd946791c863da2ce1d51a136e3cde4e9
Elshad98 <safarov.elshad007@gmail.com>
2020-07-22 11:01:23 +0500

    Added dependencies to build.gradle

--- app/src/main/java/com/example/weather/utils/Constants.java
+++ app/src/main/java/com/example/weather/utils/Constants.java
@@ -2,8 +2,14 @@ package com.example.weather.utils;
 
 public class Constants {
 
+    /**
+     * Base url for api call
+     */
     public static final String BASE_URL = "https://api.openweathermap.org/data/2.5";
 
+    /**
+     * API key required to send a request
+     */
     public static final String API_KEY = "35eb36d6c29f1e2cc642273d12bc021a";
 
 }

f6dc715acbec5a3fb86a603264de562a028a6301
Elshad98 <safarov.elshad007@gmail.com>
2020-08-13 22:07:22 +0500

    added Roboto font to the project

---
+++ app/src/main/java/com/example/weather/utils/FontUtil.java
@@ -0,0 +1,13 @@
+package com.example.weather.utils;
+
+import android.graphics.Typeface;
+
+public class FontUtil {
+
+    private static Typeface robotoLight;
+    private static Typeface robotoRegular;
+    private static Typeface robotoMedium;
+    private static Typeface robotoCondensedRegular;
+
+
+}

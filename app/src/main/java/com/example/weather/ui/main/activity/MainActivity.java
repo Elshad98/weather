81f56f1e22bdc290d792eb091a1be970b61b101a
Сафаров Эльшад Нурали оглы <elshad.safarov@domru.ru>
2020-08-15 12:06:27 +0500

    Added Roboto font to the project

--- app/src/main/java/com/example/weather/ui/main/activity/MainActivity.java
+++ app/src/main/java/com/example/weather/ui/main/activity/MainActivity.java
@@ -8,9 +8,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 import com.example.weather.R;
 import com.example.weather.data.local.entity.CityEntity;
